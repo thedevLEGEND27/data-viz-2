ff81406fa218b020918bf5d7e1f6356617677f36
Tej Sidhu <tsid0009@student.monash.edu>
2023-10-12 03:02:34 +1100

    pushhh

--- embed.js
+++ embed.js
@@ -1,4 +1,4 @@
-import file from './map.json' with { type: "json" };
+
 var vlSpec = config
 vegaEmbed('#graph2', vlSpec,{"actions": false}).then(function(result) {
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
