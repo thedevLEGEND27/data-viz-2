17ee82525be4da5ddba4e46ba64e20847f2cb519
Tej Sidhu <tsid0009@student.monash.edu>
2023-10-15 05:07:51 +1100

    graph 1

--- map.js
+++ map.js
@@ -4,3 +4,8 @@ var map1 = "https://raw.githubusercontent.com/thedevLEGEND27/data-viz-2.github.i
 vegaEmbed("#map", map1).then(function(result){
 
 }).catch(console.error);
+
+var graph1 = "https://raw.githubusercontent.com/thedevLEGEND27/data-viz-2.github.io/main/graph1.json";
+vegaEmbed("#graph1", graph1).then(function(result){
+
+}).catch(console.error);
