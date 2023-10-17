ee40410d46fe33368b0ed50b02440bf2da82a0ef
Tej Sidhu <tsid0009@student.monash.edu>
2023-10-18 04:00:00 +1100

    lastone

--- pivot.R
+++ pivot.R
@@ -1,12 +1,11 @@
 library(readr)
 library(tidyverse)
 library(tidyr)
-country_temp <- read_csv("country temp.csv")
+country_temp <- read_csv("temp_long.csv")
 View(country_temp)
-
-temps <- t(country_temp)
+temps <- pivot_longer(country_temp, 2:14 ,names_to="Observations", values_to="Temp")
 View(temps)
 
-write.csv(temps, "correct temps.csv", row.names=TRUE)
+write.csv(temps,"final_temps.csv")
 
 
