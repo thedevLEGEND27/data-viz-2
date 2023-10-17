bc6f53386b4b4bfe0dbd8f368c2565ba57158094
Tej Sidhu <tsid0009@student.monash.edu>
2023-10-18 06:04:16 +1100

    fixes

--- embed.js
+++ embed.js
@@ -16,6 +16,11 @@ vegaEmbed("#graph2", graph2).then(function(result){
 }).catch(console.error);
 
 var graph3 = "https://raw.githubusercontent.com/thedevLEGEND27/data-viz-2.github.io/main/graph3.json";
-vegaEmbed("#graph3", graph2).then(function(result){
+vegaEmbed("#graph3", graph3).then(function(result){
+
+}).catch(console.error);
+
+var graph4 = "https://raw.githubusercontent.com/thedevLEGEND27/data-viz-2.github.io/main/graph4.json";
+vegaEmbed("#graph4", graph4).then(function(result){
 
 }).catch(console.error);
